e22dcada518c33f98917e1d56b6aec1a91dc7177
eunhyeong99 <whdmsgud8@naver.com>
2024-11-30 19:12:42 +0900

    feat: websocket and stomp 실시간채팅 구현

---
+++ src/main/java/com/example/chatservice/config/WebSocketConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.chatservice.config;
+
+import com.example.chatservice.handler.WebSocketChatHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@EnableWebSocket
+@Configuration
+@RequiredArgsConstructor
+public class WebSocketConfiguration implements WebSocketConfigurer {
+
+    final WebSocketChatHandler webSocketChatHandler;
+
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketChatHandler, "/ws/chats");
+    }
+}
